fea8a6ab89c6c321bc569c954818e86855db60e2
orewaee <orewaee@gmail.com>
2023-11-07 20:10:30 +0400

    Changed version

--- src/main/java/dev/orewaee/Constants.java
+++ src/main/java/dev/orewaee/Constants.java
@@ -3,5 +3,5 @@ package dev.orewaee;
 public class Constants {
     public static final String ID = "discordauth";
     public static final String NAME = "DiscordAuth";
-    public static final String VERSION = "0.2.0";
+    public static final String VERSION = "0.2.1";
 }
