a40b69a8891ae32dbf7b69897a89d91bd8b5a480
orewaee <71517526+orewaee@users.noreply.github.com>
2024-07-04 02:04:09 +0400

    feat(velocity): plugin base added

---
+++ velocity/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+repositories {
+    maven("https://repo.papermc.io/repository/maven-public/")
+}
+
+dependencies {
+    compileOnly(libs.velocity)
+    annotationProcessor(libs.velocity)
+
+    implementation(project(":common"))
+}
+
+tasks {
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.release = 17
+    }
+
+    shadowJar {
+        archiveFileName.set("${rootProject.name}-$version.jar")
+    }
+}
---
+++ velocity/src/main/java/dev/orewaee/discordauth/velocity/DiscordAuth.java
@@ -0,0 +1,80 @@
+package dev.orewaee.discordauth.velocity;
+
+import java.io.IOException;
+
+import org.jetbrains.annotations.NotNull;
+
+import com.google.inject.Inject;
+
+import org.slf4j.Logger;
+
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+
+import dev.orewaee.discordauth.api.DiscordAuthAPI;
+import dev.orewaee.discordauth.api.account.AccountManager;
+import dev.orewaee.discordauth.common.account.JsonAccountManager;
+import dev.orewaee.discordauth.api.key.KeyManager;
+import dev.orewaee.discordauth.common.key.InMemoryKeyManager;
+import dev.orewaee.discordauth.api.pool.PoolManager;
+import dev.orewaee.discordauth.common.pool.InMemoryPoolManager;
+import dev.orewaee.discordauth.api.session.SessionManager;
+import dev.orewaee.discordauth.common.session.InMemorySessionManager;
+
+@Plugin(id = "discordauth", name = "discord-auth", version = "0.4.0", authors = {"orewaee"})
+public class DiscordAuth implements DiscordAuthAPI {
+    private static DiscordAuth instance;
+
+    private final ProxyServer proxy;
+    private final Logger logger;
+
+    private final AccountManager accountManager;
+    private final KeyManager keyManager;
+    private final PoolManager poolManager;
+    private final SessionManager sessionManager;
+
+    @Inject
+    public DiscordAuth(ProxyServer proxy, Logger logger) throws IOException {
+        instance = this;
+
+        this.proxy = proxy;
+        this.logger = logger;
+
+        this.accountManager = new JsonAccountManager();
+        this.keyManager = new InMemoryKeyManager(1000 * 10);
+        this.poolManager = new InMemoryPoolManager();
+        this.sessionManager = new InMemorySessionManager(1000 * 60);
+    }
+
+    @Override
+    public @NotNull AccountManager getAccountManager() {
+        return accountManager;
+    }
+
+    @Override
+    public @NotNull KeyManager getKeyManager() {
+        return keyManager;
+    }
+
+    @Override
+    public @NotNull PoolManager getPoolManager() {
+        return poolManager;
+    }
+
+    @Override
+    public @NotNull SessionManager getSessionManager() {
+        return sessionManager;
+    }
+
+    public ProxyServer getProxy() {
+        return proxy;
+    }
+
+    public Logger getLogger() {
+        return logger;
+    }
+
+    public static DiscordAuth getInstance() {
+        return instance;
+    }
+}
