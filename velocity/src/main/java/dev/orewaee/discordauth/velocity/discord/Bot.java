57d45326f0fbd2cb1388c60f439052b3d042461c
orewaee <orewaee@gmail.com>
2024-07-09 00:56:00 +0400

    fix: list command was not added by mistake

--- velocity/src/main/java/dev/orewaee/discordauth/velocity/discord/Bot.java
+++ velocity/src/main/java/dev/orewaee/discordauth/velocity/discord/Bot.java
@@ -6,7 +6,10 @@ import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandGroupData;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
@@ -76,7 +79,8 @@ public class Bot {
                 .addOption(OptionType.STRING, "name", "Account name", true)
                 .addOption(OptionType.USER, "discord_id", "Account discordId", true),
             Commands.slash("remove", "Remove existing account")
-                .addOption(OptionType.USER, "discord_id", "Account discordId", true)
+                .addOption(OptionType.USER, "discord_id", "Account discordId", true),
+            Commands.slash("list", "List of all accounts")
         ).queue();
     }
 }
